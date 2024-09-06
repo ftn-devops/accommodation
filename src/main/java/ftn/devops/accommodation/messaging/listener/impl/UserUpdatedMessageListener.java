45c7b7d93538cb0a2c69baedb3adfc55953bbb6c
Dusan Brkic <dusanbrk@gmail.com>
2024-09-06 19:28:37 +0200

    feature: update and create user on message

--- src/main/java/ftn/devops/accommodation/messaging/listener/impl/UserUpdatedMessageListener.java
+++ src/main/java/ftn/devops/accommodation/messaging/listener/impl/UserUpdatedMessageListener.java
@@ -1,19 +1,25 @@
 package ftn.devops.accommodation.messaging.listener.impl;
 
-import ftn.devops.accommodation.messaging.listener.IMessageListener;
-import ftn.devops.accommodation.messaging.messages.UserUpdatedMessage;
-import ftn.devops.accommodation.util.constants.UserMessagingConstants.Queue;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import ftn.devops.accommodation.messaging.listener.IMessageListener;
+import ftn.devops.accommodation.messaging.messages.UserUpdatedMessage;
+import ftn.devops.accommodation.service.UserService;
+import ftn.devops.accommodation.util.constants.UserMessagingConstants.Queue;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class UserUpdatedMessageListener implements IMessageListener<UserUpdatedMessage> {
+
+    private final UserService userService;
 
     @Override
     @RabbitListener(queues = Queue.USER_UPDATE)
     public void receiveMessage(UserUpdatedMessage message) {
-        log.info("Received user update message {}", message);
+       userService.updateUser(message);
     }
 }
